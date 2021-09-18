e7e1d590f170d95dad9200d0cb8d4c610722bf4a
Vitalij Ryndin <sys@crashmax.ru>
2021-09-18 12:06:21 +0800

    ref: isURL

--- lib/valid-url.ts
+++ lib/valid-url.ts
@@ -1,33 +1,30 @@
 import * as yup from 'yup'
 import validator from 'validator'
 
-const isValidUrl = (host?: string) => yup.object({
-  url: yup.string()
-    .label('URL')
-    .required()
-    .trim()
-    .test(
-      'is-url',
-      (value) => {
-        try {
-          const url = new URL(value)
-
-          if (url.host === (host || window.location.host)) {
-            return false
-          }
-
-          return validator.isURL(value, {
-            protocols: ['http', 'https']
-          })
-        } catch (_) {
-          return false
-        }
-      }
-    )
-})
-
 const isURL = (url: string, host?: string) => {
-  return isValidUrl(host).isValidSync({ url })
+  return yup.object({
+    url: yup
+      .string()
+      .trim()
+      .required()
+      .test('is-url', value => testURL(value, host))
+  }).isValidSync({ url })
+}
+
+const testURL = (value: string, host?: string) => {
+  try {
+    const url = new URL(value)
+
+    if (url.host === (host || window.location.host)) {
+      return false
+    }
+
+    return validator.isURL(value, {
+      protocols: ['http', 'https']
+    })
+  } catch (_) {
+    return false
+  }
 }
 
 export default isURL
